7c587e328fb95e887f4ebc86f5ecee3a3f1a2d5d
Bogdan <bsshul@gmail.com>
2023-01-24 01:11:15 +0200

    Modal finished

--- components/GoodCard.tsx
+++ components/GoodCard.tsx
@@ -82,201 +82,105 @@ export default function GoodCard({ product }: Props) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="flex max-w-screen-lg w-full h-screen md:h-auto flex-col bg-white p-4 transform rounded-none md:rounded-3xl">
+              <Dialog.Panel className="flex w-screen sm:max-w-5xl h-screen md:h-[600px] bg-white transform rounded-none md:rounded-3xl overflow-hidden">
                 <Image
                   onClick={closeModal}
-                  className="absolute top-5 right-5 cursor-pointer z-30"
+                  className="absolute top-5 right-5 opacity-75 hover:opacity-100 transition-all cursor-pointer z-30"
                   src="/closeBtn.svg"
                   alt="Close"
                   priority
                   width={20}
                   height={20}
                 />
-                <main className="flex-grow mt-auto flex flex-col md:flex-row gap-3">
-                  <div className="img md:w-[40%] h-1/3 md:h-auto flex justify-center">
-                    <Image
-                      className="object-contain"
-                      src={product.images[0]}
-                      alt={product.title}
-                      priority
-                      width={400}
-                      height={400}
-                    />
-                  </div>
-                  <div className="descr bg-slate-400 p-2 flex-grow overflow-auto text-left">
+                <main className="w-full h-full flex flex-col p-4 md:flex-row gap-3">
+                  <Image
+                    className="w-full md:w-1/2 h-1/3 md:h-auto object-contain rounded-2xl"
+                    src={product.images[0]}
+                    alt={product.title}
+                    priority
+                    width={400}
+                    height={400}
+                  />
+                  <div className="w-full h-2/3 md:h-auto md:w-1/2 flex flex-col text-left">
                     <h3 className="text-2xl font-medium leading-6 text-black">
                       {product.title}
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      {product.size?.s2},{" "}
-                      {product.doughThickness?.traditional.toLowerCase()}
-                    </p>
                     <p className="my-2 text-sm text-black">
                       {product.description}
                     </p>
-
-                    {product.category === "pizza" && (
-                      <div className="filterWrapper">
-                        <div className="w-1/3 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
-                        <div
-                          className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
-                        >
-                          <span className="cursor-pointer hover:text-black transition-all">
-                            {product.size?.s1}
-                          </span>
-                          <span className="cursor-pointer hover:text-black transition-all">
-                            {product.size?.s2}
-                          </span>
-                          <span className="cursor-pointer hover:text-black transition-all">
-                            {product.size?.s3}
-                          </span>
-                        </div>
-                      </div>
-                    )}
-
-                    {product.category === "pizza" && (
-                      <div className="filterWrapper">
-                        <div className="w-1/2 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
-                        <div
-                          className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
-                        >
-                          <span className="cursor-pointer hover:text-black transition-all">
-                            {product.doughThickness?.think}
-                          </span>
-                          <span className="cursor-pointer hover:text-black transition-all">
-                            {product.doughThickness?.traditional}
-                          </span>
-                        </div>
-                      </div>
-                    )}
-                    {product.category === "pizza" && (
-                      <h2 className="mt-4 mb-2 text-xl font-medium text-black">
-                        Додатково замовити
-                      </h2>
-                    )}
-                  </div>
-                </main>
-                <button
-                  type="button"
-                  className="mt-4 rounded-2xl border border-transparent bg-orange-500 px-8 py-3 text-base font-normal text-white hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 whitespace-nowrap"
-                  onClick={closeModal}
-                >
-                  Додати в кошик за {product.price} грн
-                </button>
-              </Dialog.Panel>
-              {/* <Image
-                    onClick={closeModal}
-                    className="absolute top-5 right-5 cursor-pointer z-30"
-                    src="/closeBtn.svg"
-                    alt="Close"
-                    priority
-                    width={20}
-                    height={20}
-                  />
-                  <div className="flex h-screen sm:h-auto md:pb-6 sm:pb-0 gap-0 sm:gap-6 flex-col md:flex-row">
-                    <img
-                      className="block text-center rounded-xl object-cover sticky top-0 sm:relative"
-                      src={product.images[0]}
-                      alt={product.title}
-                    />
-
-                    <div className="w-full md:w-1/2 bg-white flex flex-col max-h-[65vh] overflow-auto flex-grow">
-                      <div className="mt-2 pr-4 sm:my-8 flex flex-col overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-200">
-                        <Dialog.Title
-                          as="h3"
-                          className="text-2xl font-medium leading-6 text-black"
-                        >
-                          {product.title}
-                        </Dialog.Title>
-
-                        <p className="mt-1 text-sm text-gray-500">
-                          {product.size?.s2},{" "}
-                          {product.doughThickness?.traditional.toLowerCase()}
-                        </p>
-                        <p className="my-2 text-sm text-black">
-                          {product.description}
-                        </p>
-
-                        {product.category === "pizza" && (
-                          <div className="filterWrapper">
-                            <div className="w-1/3 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
-                            <div
-                              className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
-                            >
-                              <span className="cursor-pointer hover:text-black transition-all">
-                                {product.size?.s1}
-                              </span>
-                              <span className="cursor-pointer hover:text-black transition-all">
-                                {product.size?.s2}
-                              </span>
-                              <span className="cursor-pointer hover:text-black transition-all">
-                                {product.size?.s3}
-                              </span>
-                            </div>
+                    <div className="flex-grow pr-4 mb-2 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-200">
+                      {product.category === "pizza" && (
+                        <div className="filterWrapper">
+                          <div className="w-1/3 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
+                          <div
+                            className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
+                          >
+                            <span className="cursor-pointer hover:text-black transition-all">
+                              {product.size?.s1}
+                            </span>
+                            <span className="cursor-pointer hover:text-black transition-all">
+                              {product.size?.s2}
+                            </span>
+                            <span className="cursor-pointer hover:text-black transition-all">
+                              {product.size?.s3}
+                            </span>
                           </div>
-                        )}
-
-                        {product.category === "pizza" && (
-                          <div className="filterWrapper">
-                            <div className="w-1/2 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
-                            <div
-                              className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
-                            >
-                              <span className="cursor-pointer hover:text-black transition-all">
-                                {product.doughThickness?.think}
-                              </span>
-                              <span className="cursor-pointer hover:text-black transition-all">
-                                {product.doughThickness?.traditional}
-                              </span>
-                            </div>
-                          </div>
-                        )}
-                        {product.category === "pizza" && (
-                          <h2 className="mt-4 mb-2 text-xl font-medium text-black">
-                            Додатково замовити
-                          </h2>
-                        )}
-
-                        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 py-2">
-                          {product.additionalGoods?.map((additional) => (
-                            <div
-                              key={additional.id}
-                              className="p-2 shadow-[0_7px_7px_-5px_rgba(0,0,0,0.15)] rounded-xl bg-white border border-gray-200"
-                            >
-                              <Image
-                                className="rounded-xl mb-2"
-                                src={additional.image}
-                                alt={additional.title}
-                                priority
-                                width={300}
-                                height={300}
-                              />
-
-                              <h3 className="text-sm">{additional.title}</h3>
-                              <p className="text-center font-medium">
-                                {additional.price} грн
-                              </p>
-                            </div>
-                          ))}
                         </div>
+                      )}
+
+                      {product.category === "pizza" && (
+                        <div className="filterWrapper">
+                          <div className="w-1/2 h-8 bg-white shadow-[0_3px_12px_-7px_rgba(0,0,0,0.7)] py-1 px-6 rounded-2xl transition-all duration-500" />
+                          <div
+                            className={`absolute top-1/2 left-0 -translate-y-1/2 w-full flex justify-around text-gray-500 py-1 z-10 transition-all duration-500 select-none`}
+                          >
+                            <span className="cursor-pointer hover:text-black transition-all">
+                              {product.doughThickness?.think}
+                            </span>
+                            <span className="cursor-pointer hover:text-black transition-all">
+                              {product.doughThickness?.traditional}
+                            </span>
+                          </div>
+                        </div>
+                      )}
+                      {product.category === "pizza" && (
+                        <h2 className="mt-4 mb-2 text-xl font-medium text-black">
+                          Додатково замовити
+                        </h2>
+                      )}
+                      <div className="grid grid-cols-4 md:grid-cols-4 gap-2">
+                        {product.additionalGoods?.map((additional) => (
+                          <div
+                            key={additional.id}
+                            className="p-2 shadow-[0_7px_7px_-5px_rgba(0,0,0,0.15)] rounded-xl bg-white border border-gray-200"
+                          >
+                            <Image
+                              className="rounded-xl mb-2"
+                              src={additional.image}
+                              alt={additional.title}
+                              priority
+                              width={200}
+                              height={200}
+                            />
+
+                            <h3 className="text-sm">{additional.title}</h3>
+                            <p className="text-center font-medium">
+                              {additional.price} грн
+                            </p>
+                          </div>
+                        ))}
                       </div>
                     </div>
                     <button
                       type="button"
-                      className="sm:hidden w-full my-2 inline-flex justify-center rounded-full border border-transparent bg-orange-500 px-8 py-3 text-base font-normal text-white hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 whitespace-nowrap"
+                      className="w-full rounded-2xl border border-transparent bg-orange-500 px-8 py-3 text-base font-normal text-white hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 whitespace-nowrap"
                       onClick={closeModal}
                     >
                       Додати в кошик за {product.price} грн
                     </button>
                   </div>
-                  <button
-                    type="button"
-                    className="hidden sm:block w-full mt-4 justify-center rounded-full border border-transparent bg-orange-500 px-8 py-3 text-base font-normal text-white hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 whitespace-nowrap"
-                    onClick={closeModal}
-                  >
-                    Додати в кошик за {product.price} грн
-                  </button>
-                </Dialog.Panel> */}
+                </main>
+              </Dialog.Panel>
             </Transition.Child>
           </div>
         </Dialog>
